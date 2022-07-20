88c05630a3b1e819423e0d6743b7b7bbfb2e7723
Suna Ulus <suna.ulus@gmail.com>
2022-07-19 23:01:56 -0500

    solved the input exercises

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,9 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+   addMovie (newTitle: string) {
+      if(!this.movies.includes(newTitle) && newTitle !== ""){
+         this.movies.push(newTitle);
+      }
+   }
 }
